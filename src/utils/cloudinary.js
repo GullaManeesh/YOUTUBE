31ac3c87c4fb7ac01ccf513a00211dcffafa901b
Gulla Maneesh <gullamanish2045@gmail.com>
2025-06-21 19:11:12 +0530

    allsetup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload fileon cloudinary from local file
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: auto,
+    });
+    //file uploaded and getting url
+    console.log("file is uploaded on cludinary:", response.url);
+    return response;
+  } catch (error) {
+    //if file is not uploaded remove locally saved temporary file
+    fs.unlinkSync(localFilePath);
+    console.error("Error uploading file to Cloudinary:", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
